f347824b404bdaa870ed2661e7ca27681add368c
DrPaul1982 <avtoklavir@ukr.net>
2024-10-16 23:04:18 +0300

    Add changes in Main and Corrector files Add gitignore

---
+++ src/app/DataProvider.java
@@ -0,0 +1,7 @@
+package src.app;
+
+public class DataProvider extends Corrector {
+    public String[] getData() {
+        return new String[]{"brange", "plum", "tomato", "onibn", "grape"};
+    }
+}
